d29e94c31b7640e6a28ee2ab1ef5396a2f028fab
anisonley <anisonley@gmail.com>
2023-06-22 21:11:18 +0100

    update on responsive

--- sprout-tutor/src/Components/TeacherComponents/TeacherText/TeacherText.jsx
+++ sprout-tutor/src/Components/TeacherComponents/TeacherText/TeacherText.jsx
@@ -30,16 +30,21 @@ const TeacherText = () => {
   return (
     <div>
       <label>
-        <h1>Lesson Title</h1>
+        <h2>Lesson Title</h2>
           <input type="text"  name='text' placeholder="Untitled Title" value={lessonTitle} onChange={handleTitleChange} />
       </label>
 
       <label>
-          <h1>Course Description</h1>
+          <h2>Lesson Description (add a link here)</h2>
           <textarea name='text' id='description' placeholder="What is the Lesson about" value={lessonDescription} onChange={handleDescriptionChange} />
 
       </label>
-      <Dropzone onDrop={handleFileDrop} multiple>
+
+      <span>
+        <h2>Text File</h2>
+      </span>
+      <div>
+        <Dropzone onDrop={handleFileDrop} multiple>
         {({ getRootProps, getInputProps }) => (
           <div
             {...getRootProps()}
@@ -53,14 +58,14 @@ const TeacherText = () => {
             <p>Drag and drop document files here, or click to select files</p>
           </div>
         )}
-      </Dropzone>
-      {documentFiles.map((file, index) => (
-        <div key={index}>
-          <p>{file.name}</p>
-        </div>
-      ))}
-     
-      <button onClick={handleSave}>Save</button>
+        </Dropzone>
+        {documentFiles.map((file, index) => (
+          <div key={index}>
+            <p>{file.name}</p>
+          </div>
+        ))}
+      </div>
+      <button onClick={handleSave}>Publish</button>
     </div>
   );
 };
