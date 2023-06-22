fb45796c09da227e629d2cdecd4e444b132a5855
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2023-06-22 04:23:38 +0100

    revamped landing page

--- sprout-tutor/src/Components/Offers/Offers.jsx
+++ sprout-tutor/src/Components/Offers/Offers.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Offers.scss'
 import OfferSlide from '../Slides/OfferSlide/OfferSlide'
+import heading from '../../assets/heading.png'
 
 
 const Offers = () => {
   return (
     <div id='offers'>
+      <img src={heading} alt="" className="heading" />
         <h1>What we offer at Sprout Tutor</h1>
         <OfferSlide />
     </div>
